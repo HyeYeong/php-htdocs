0fa00ee9fb5b96207ae3ad603c506f4761de1e43
HyeYeong <todzlfk@naver.com>
2024-12-15 13:18:53 +0900

    feat: var dump and variables

--- pages/helloworld.php
+++ pages/helloworld.php
@@ -1,3 +1,17 @@
-<?php
-    echo "Hello world";
-?>
+<html>
+  <h1>
+    <?php
+      $thisIsVariable = 'this is variable !';
+      $thisIsNumber = 16;
+      echo "Hello world. ".$thisIsVariable ." ";
+      echo var_dump($thisIsVariable)
+    ?>
+  </h1>
+  <p>
+    <?php
+      echo $thisIsVariable;
+      echo var_dump($thisIsNumber);
+      echo var_dump($thisIsNumber/5);
+    ?>
+  </p>
+</html>
