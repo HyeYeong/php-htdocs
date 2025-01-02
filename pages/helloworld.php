7d51f6c69561f4cd28a7de38ee466fc60b039491
HyeYeong <todzlfk@naver.com>
2025-01-02 21:09:09 +0900

    feat: study array

--- pages/helloworld.php
+++ pages/helloworld.php
@@ -3,7 +3,7 @@
     <?php
       $thisIsVariable = 'this is variable !';
       $thisIsNumber = 16;
-      echo "Hello world. ".$thisIsVariable ." ";
+      echo "Hello world{$thisIsVariable}-!";
       echo var_dump($thisIsVariable);
       echo gettype($thisIsNumber);
       
@@ -29,4 +29,26 @@
     password: <input type="password" name="password"/>
     <input type="submit"/>
   </form>
+
+  <?php
+      $array1 = ["tim", "kim", "him", "dim", "aim", "bim", "gim"];
+      echo count($array1);
+      array_push($array1, 'mit');
+      echo count($array1);
+      array_pop($array1);
+      echo count($array1);
+      array_shift($array1);
+      echo count($array1);
+      array_unshift($array1, "Mint");
+      rsort($array1);
+      echo var_dump($array1);
+
+      function changeFirstAlphabet ($arr) {
+        for($i=0; $i<count($arr); $i++) {
+          sort($arr);
+          echo ucfirst($arr[$i]);
+        }
+      }
+      changeFirstAlphabet($array1);
+    ?>
 </html>
